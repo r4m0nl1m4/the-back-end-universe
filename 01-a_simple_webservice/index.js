40390fc384a430c368c9797dbdd7e297b6c074cc
Paulo Ramon <paulo.ramon@neoenergia.com>
2022-08-08 22:08:36 -0300

    a webservice with paths interpretation

--- 01-a_simple_webservice/index.js
+++ 01-a_simple_webservice/index.js
@@ -1,12 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.writeHead(200,{'Content-Type': "text/json"})
-    res.write(JSON.stringify({ msg: "Hello World!!"}))
-    res.end()
+  console.log(req.url)
+  res.writeHead(200,{'Content-Type':"text/json"})
+  res.write(JSON.stringify({ msg: "Hello World!!"}))
+  res.end()
 })
 
 server.listen(8080, () => {
-    console.log('Server ready on port 8080!')
+  console.log('Server ready on port 8080!')
 })
